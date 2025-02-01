5f91d60888d66e8b2a85af59a73745a1bac69cc7
Yebo <froshmedia@gmail.com>
2025-02-01 13:39:40 +0100

    first cut..

--- components/Product.jsx
+++ components/Product.jsx
@@ -17,7 +17,6 @@ const Product = ({ product: { image, name, slug, price } }) => {
           />
           <p className="product-name">{name}</p>
           <p className="product-price">N{price}</p>
-          <p className="product-price">{description}</p>
         </div>
       </Link>
     </div>
